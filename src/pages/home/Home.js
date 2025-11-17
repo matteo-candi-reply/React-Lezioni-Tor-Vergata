25e23110a7f30919d13895b2c2b1af1181bc2b1e
m.candi <m.candi@reply.it>
2025-11-17 09:39:56 +0100

    clean code for lesson

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,8 @@
-import Header from "../../components/Header";
-import TimetableGrid from "../../components/TimetableGrid";
-import CommunicationList from "../../components/CommunicationList";
-
-import communicationsData from '../../mock/communications.json';
-import timetableData from './../../mock/timetable.json';
-
 
 function Home() {
 
     return (
         <>  
-            <Header
-                title={"Sistema Universitario"}
-                subtitle={"Portale Studenti"}
-            />
-
-            <CommunicationList 
-                communications={communicationsData}
-            />
-
-            <TimetableGrid 
-                data={timetableData}
-            />
         </>
     )
 
